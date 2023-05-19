14a18c1aa94770e41c861bb6989d3b60b499cfb8
onban <onban@DESKTOP-V3VNOEI>
2023-05-19 18:34:37 +0900

    게시물 수정 기능 추가

--- Board/src/main/java/com/board/mapper/BoardMapper.java
+++ Board/src/main/java/com/board/mapper/BoardMapper.java
@@ -12,6 +12,9 @@ public interface BoardMapper {
 	/*게시판 등록*/
 	public void enroll (BoardVO board);
 	
-
+	/*게시판 수정*/
+	//수정 데이터가 필요하기때문에 BoardVO를 파라미터로 지정.
+	//수정을 수행하는 메소드의 경우 반환 타입이 필요없지만 INT로 설정 하면 성공시 1, 실패시0 반환
+	public int modify(BoardVO board);
 
 }
